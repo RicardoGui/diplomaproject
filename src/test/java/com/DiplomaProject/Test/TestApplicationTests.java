9ae3fd33bd039d64beba3c46d2e1fb1b3ac73db3
Ricardo <wenqiang.stu@gmail.com>
2022-01-12 09:58:07 +0800

    2-2 init

---
+++ src/test/java/com/DiplomaProject/Test/TestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.DiplomaProject.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
