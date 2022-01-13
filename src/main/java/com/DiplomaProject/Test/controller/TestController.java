ff27b96dac2b3dd99d18a60ecd3a1fd584f514f9
Ricardo <wenqiang.stu@gmail.com>
2022-01-13 08:59:45 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/DiplomaProject/Test/controller/TestController.java
+++ src/main/java/com/DiplomaProject/Test/controller/TestController.java
@@ -14,4 +14,10 @@ public class TestController {
     public String   hello(){
         return "Hello World!";
     }
+    @PostMapping("/hello/post")
+    public String   helloPost(String name){
+        return "Hello World!Post,"+ name;
+    }
 }
+
+
